e3e364cc2a4b967ee423d68fc24772f21754fb98
nataliesleight <nsleight@uwyo.edu>
2025-04-13 22:04:23 -0600

    Update code.js

--- code.js
+++ code.js
@@ -2,23 +2,15 @@ function hasCycle(graph) {
     var visited = [];
     var parent = "null";
     for (var i = 0; i < graph.length; i++) {    // check each unvisited node, isolated ones too
-        if (!checkVisit(i,visited) && detectCycle(graph, i, visited, parent)) {  // if not visited and has cycle
+        if (!visited.includes(i) && detectCycle(graph, i, visited, parent)) {  // if not visited and has cycle
             return true;
         }
     }
     return false;
 }
 
-function checkVisit(node, visited) {
-    if (visited.length == 0) {return false;}
-    for (var i = 0; i < visited.length; i++) {
-        if (node == visited[i]) {return true;}
-    }
-    return false;
-}
-
 function detectCycle(graph, node, visited, parent) {
-    if (checkVisit(node, visited)) {return true;}        // node already visited, cycle (check within recursion)
+    if (visited.includes(node)) {return true;}        // node already visited, cycle (check within recursion)
     visited.push(node);                         // node not visited, add to visited list
     for (var i = 0; i < graph[node].length; i++) {    // check each edge from node
         if (graph[node][i] != parent && detectCycle(graph,graph[node][i],visited,node)) {return true;}    // if descendant not parent
