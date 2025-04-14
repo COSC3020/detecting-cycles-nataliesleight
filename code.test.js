d1d4e7a1cd0595534225b0ed43289603747814ff
nataliesleight <nsleight@uwyo.edu>
2025-04-13 21:54:14 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -3,22 +3,53 @@ const jsc = require('jsverify');
 
 eval(fs.readFileSync('code.js')+'');
 
-var testGraph1 = [];
+var testGraph1 = [    // example graph, has cycle 0-1-3
+  [1,3,5],
+  [0,2,3],
+  [1],
+  [0,1,4,5],
+  [3,5],
+  [0,4,3]
+];
 var result = hasCycle(testGraph1);
-jsc.assert(JSON.stringify(result) == JSON.stringify([true));
+jsc.assert(JSON.stringify(result) == JSON.stringify(true));
 
-var testGraph2 = [];
+var testGraph2 = [  // testing isolation 
+  [1],
+  [0],
+  [],
+  [4],
+  [3]
+];
 var result = hasCycle(testGraph2);
-jsc.assert(JSON.stringify(result) == JSON.stringify([true));
+jsc.assert(JSON.stringify(result) == JSON.stringify(false));
 
-var testGraph3 = [];
+var testGraph3 = [ // no cycles in connected graph
+  [1,2],
+  [0,4],
+  [0,3],
+  [2],
+  [1]
+];
 var result = hasCycle(testGraph3);
-jsc.assert(JSON.stringify(result) == JSON.stringify([true));
+jsc.assert(JSON.stringify(result) == JSON.stringify(false));
 
-var testGraph4 = [];
+var testGraph4 = [
+  [4],
+  [3],
+  [2],
+  [1],
+  [0]
+];
 var result = hasCycle(testGraph4);
-jsc.assert(JSON.stringify(result) == JSON.stringify([true));
+jsc.assert(JSON.stringify(result) == JSON.stringify(true));
 
-var testGraph5 = [];
+var testGraph5 = [  // isolated graph, cycle exists
+  [3],
+  [2,4],
+  [1,4],
+  [0],
+  [1,2]
+];
 var result = hasCycle(testGraph5);
-jsc.assert(JSON.stringify(result) == JSON.stringify([true));
+jsc.assert(JSON.stringify(result) == JSON.stringify(true));
